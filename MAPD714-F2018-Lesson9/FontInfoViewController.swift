9fe0185a1613efe72e95a5bfa6771e0edf773875
harshkeshwala <keshwalaharsh@gmail.com>
2018-11-16 22:11:49 -0500

    Font Size Label and Font Label Error Resolved

--- MAPD714-F2018-Lesson9/FontInfoViewController.swift
+++ MAPD714-F2018-Lesson9/FontInfoViewController.swift
@@ -12,7 +12,6 @@ class FontInfoViewController: UIViewController {
 
     var font: UIFont!
     var favorite: Bool = false
-    let segueIdentifier = "FontInfo"
     
     @IBOutlet weak var fontSampleLabel: UILabel!
     @IBOutlet weak var fontSizeSlider: UISlider!
@@ -27,14 +26,14 @@ class FontInfoViewController: UIViewController {
      fontSampleLabel.text = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVv"
             + "WwXxYyZz 0123456789"
         fontSizeSlider.value = Float(font.pointSize)
-        fontSampleLabel.text = "\(Int(font.pointSize))"
+        fontSizeLabel.text = "\(Int(font.pointSize))"
         favoriteSwitch.isOn = favorite
     }
     
     @IBAction func slideFontSize(slider: UISlider){
         let newSize = roundf(slider.value)
         fontSampleLabel.font = font.withSize(CGFloat(newSize))
-        fontSampleLabel.text = "\(Int(newSize))"
+        fontSizeLabel.text = "\(Int(newSize))"
     }
     
     @IBAction func toggleFavorite(sender: UISwitch){
